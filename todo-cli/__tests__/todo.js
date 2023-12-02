20e43c40fcb084fb1b60dc724a3321aef3f83acc
root <root@Jagadheesh>
2023-12-02 12:58:17 +0530

    Add lint staged

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,22 +1,60 @@
 /* eslint-disable */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("Todo list testsuite", () => {
+  beforeAll(() => {
+    const formattedDate = (d) => {
+      return d.toISOString().split("T")[0];
+    };
+
+    var dateToday = new Date();
+    const today = formattedDate(dateToday);
+    const yesterday = formattedDate(
+      new Date(new Date().setDate(dateToday.getDate() - 1)),
+    );
+    const tomorrow = formattedDate(
+      new Date(new Date().setDate(dateToday.getDate() + 1)),
+    );
+
+    add({ title: "Submit assignment", dueDate: yesterday, completed: false });
+    add({ title: "Pay rent", dueDate: today, completed: true });
+    add({ title: "Service Vehicle", dueDate: today, completed: false });
+    add({ title: "File taxes", dueDate: tomorrow, completed: false });
+  });
   test("Should add new todo", () => {
-    expect(all.length).toBe(0);
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toISOString().slice(0, 10),
-    });
-    expect(all.length).toBe(1);
+    const formattedDate = (d) => {
+      return d.toISOString().split("T")[0];
+    };
+    var dateToday = new Date();
+    const tomorrow = formattedDate(
+      new Date(new Date().setDate(dateToday.getDate() + 1)),
+    );
+    let len = all.length;
+    expect(all.length).toBe(len);
+    add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
+    expect(all.length).toBe(len + 1);
   });
 
   test("Should mark as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+    expect(all[4].completed).toBe(false);
+    markAsComplete(4);
+    expect(all[4].completed).toBe(true);
+  });
+
+  test("Retrieval of overdue items", () => {
+    const overdueList = overdue();
+    expect(overdueList.length).toBe(1);
+  });
+
+  test("Retrieval of due today items", () => {
+    const dueTodayList = dueToday();
+    expect(dueTodayList.length).toBe(2);
+  });
+
+  test("Retrieval of due later items", () => {
+    const dueLaterList = dueLater();
+    expect(dueLaterList.length).toBe(2);
   });
 });
